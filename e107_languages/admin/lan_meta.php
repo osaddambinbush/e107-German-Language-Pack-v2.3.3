3993706d0e9e96e078a99dc2316d409cbd547ea4
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-05 23:29:08 +0200

    Add files via upload

---
+++ e107_languages/admin/lan_meta.php
@@ -0,0 +1,30 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - german language file
+|     UTF-8 encoded
+|     translated for: http://www.e107cms.de
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $HeadURL: https://svn.code.sf.net/p/e107german/code/trunk/e107_0.7/e107_languages/German/admin/lan_meta.php $ 
+|     $Revision: 115 $
+|     $Date: 2010-09-26 06:00:45 +0200 (So, 26. Sep 2010) $
+|     $Id: lan_meta.php 115 2010-09-26 04:00:45Z lars78 $
+|     $Author: lars78 $
++----------------------------------------------------------------------------+
+*/
+define("METLAN_00", "Meta Tags");
+define("METLAN_1", "Meta Tags in Datenbank aktualisiert");
+define("METLAN_2", "Meta Tags eingeben");
+define("METLAN_3", "Eingabe neuer Meta Tag Einstellungen");
+
+define("METLAN_4", "Custom tags (inside [x] tags)");
+define("METLAN_5", "Custom tags (after [x])");
+define("METLAN_6", "Custom tags (before [x])");
+define("METLAN_7", "Any meta data or custom HTML tags entered here (such as <script> tags or Google analytics code) will be included on every page of the website in their designated areas.");
+
+define("METLAN_8", "SEO Title Character Limit");
+define("METLAN_9", "SEO Description Character Limit");
+
