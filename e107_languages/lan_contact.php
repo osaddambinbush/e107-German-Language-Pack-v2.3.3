ec604f37c4b81bb4d59660f65f146d99c8e1b7cb
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-05 23:27:43 +0200

    Add files via upload

---
+++ e107_languages/lan_contact.php
@@ -0,0 +1,72 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - german language file
+|     UTF-8 encoded
+|     translated for: http://www.e107cms.de
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $HeadURL: https://svn.code.sf.net/p/e107german/code/trunk/e107_0.7/e107_languages/German/lan_contact.php $ 
+|     $Revision: 115 $
+|     $Date: 2010-09-26 06:00:45 +0200 (So, 26. Sep 2010) $
+|     $Id: lan_contact.php 115 2010-09-26 04:00:45Z lars78 $
+|     $Author: lars78 $
++----------------------------------------------------------------------------+
+*/
+define("LANCONTACT_00", "kontaktiere uns");
+define("LANCONTACT_01", "Impressum & Kontakt");
+define("LANCONTACT_02", "Kontaktformular");
+define("LANCONTACT_03", "Benutzername:");
+define("LANCONTACT_04", "E-Mail Adresse:");
+define("LANCONTACT_05", "Betreff:");
+define("LANCONTACT_06", "Nachricht:");
+define("LANCONTACT_07", "Eine Kopie dieser Nachricht an Ihre E-Mail Adresse senden");
+define("LANCONTACT_08", "Senden");
+define("LANCONTACT_09", "Ihre Nachricht wurde versand.");
+define("LANCONTACT_10", "Es gab ein Problem beim Versenden Ihrer Nachricht.");
+define("LANCONTACT_11", "Ihre E-Mail Adresse scheint nicht gültig zu sein.Bitte überprüfen Sie diese und versuchen es erneut.");
+define("LANCONTACT_12", "Ihre Nachricht ist zu kurz.");
+define("LANCONTACT_13", "Bitte fügen Sie einen Betreff ein.");
+define("LANCONTACT_14", "Nachricht senden an:");
+define("LANCONTACT_15", "Falscher Code eingegeben");
+define("LANCONTACT_16", "Sie müssen [ registriert] und angemeldet sein , um diese Formular zu verwenden.");
+define("LANCONTACT_17", "Bitte geben Sie Ihren Namen ein.");
+define("LANCONTACT_18", "Geben Sie bitte Ihre Email-Adresse ein.");
+define("LANCONTACT_19", "Bitte geben Sie den Betreff für Ihre E-Mail ein.");
+define("LANCONTACT_20", "Bitte geben Sie Ihre Nachricht für uns ein.");
+define("LANCONTACT_21", "Ich bin damit einverstanden, dass diese Website meine übermittelten Informationen speichert, damit sie auf meine Anfrage antworten können");
+define("LANCONTACT_22", "Datenschutz Bestimmungen");
+define("LANCONTACT_23", "Unsere Datenschutzbestimmungen finden Sie hier: [x]");
+define("LANCONTACT_24", "GDPR Vereinbarung");
+
+
+
+/* newer Versions uses different Syntax */
+define("LAN_CONTACT_00", "Kontaktieren Sie uns");
+define("LAN_CONTACT_01", "Nähere Informationen");
+define("LAN_CONTACT_02", "Kontaktformular");
+define("LAN_CONTACT_03", "Geben Sie Ihren Namen ein");
+define("LAN_CONTACT_04", "Geben Sie Ihre Email-Adresse ein");
+define("LAN_CONTACT_05", "Betreff");
+define("LAN_CONTACT_06", "Nachricht");
+define("LAN_CONTACT_07", "Eine Kopie per Email erhalten");
+define("LAN_CONTACT_08", "Absenden");
+define("LAN_CONTACT_09", "Ihre Nachricht wurde abgesendet.");
+define("LAN_CONTACT_10", "Fehler beim absenden Ihrer Nachricht.");
+define("LAN_CONTACT_11", "Bitte prüfen Sie Ihrer Email-Adresse und versuchen Sie es erneut.");
+define("LAN_CONTACT_12", "Ihre Nachricht ist zu kurz.");
+define("LAN_CONTACT_13", "Bitte fügen Sie einen Betreff ein.");
+
+define("LAN_CONTACT_14", "Sende Nachricht an");
+define("LAN_CONTACT_15", "Falschen code eingegeben");
+define("LAN_CONTACT_16", "Sie müssen [registered] und angemeldet sein um dieses Formular nutzen zu können.");
+define("LAN_CONTACT_17", "Bitte geben Sie Ihren Namen ein.");
+define("LAN_CONTACT_18", "Bitte geben Sie Ihre Email-Adresse ein.");
+define("LAN_CONTACT_19", "Bitte geben Sie einen Betreff für Ihre Nachricht ein.");
+define("LAN_CONTACT_20", "Ihre Nachricht an uns.");
+define("LAN_CONTACT_21", "Ich aktezptiere, dass meine Daten gespeichert werden"); //   By using this form, you agree to the storage and processing of your data through this site.");
+define("LAN_CONTACT_22", "Datenschutzerklärung");
+define("LAN_CONTACT_23", "Sie finden unsere Datenschutzerklärung hier: [x]");
+define("LAN_CONTACT_24", "Datenschutz-Grundverordnung - DSGVO");
