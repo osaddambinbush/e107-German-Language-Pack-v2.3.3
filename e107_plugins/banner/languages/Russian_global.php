05b03566d04cca185e8ee7e408686ec5b5016d79
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-07 20:04:06 +0200

    Add files via upload

---
+++ e107_plugins/banner/languages/Russian_global.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+define("LAN_PLUGIN_BANNER_NAME", "Баннеры");
+define("LAN_PLUGIN_BANNER_DESCRIPTION", "Добавляет рекламные баннеры");
+
+?>
