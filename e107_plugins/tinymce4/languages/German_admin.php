f0a56d961bac635e7e413ee2535e15d2aee56141
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-07 20:41:24 +0200

    Add files via upload

---
+++ e107_plugins/tinymce4/languages/German_admin.php
@@ -0,0 +1,20 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system German Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/03/10 18:22:27
+|
+|        $Author: yak $
++---------------------------------------------------------------+
+*/
+define("TMCEALAN_1", "Füge den Text standardmäßig ein");
+define("TMCEALAN_2", "Browser Rechtschreibprüfung");
+define("TMCEALAN_3", "Aktiviere dies, wenn im internen Browser Rechtschreibprüfung verwendet werden soll.");
+define("TMCEALAN_4", "Visuelle Funktionsblöcke");
+define("TMCEALAN_5", "Aktivieren um HTML Funktionsblöcke während der Bearbeitung sichtbar zu machen.");
+define("TMCEALAN_6", "Code-Highlight CSS class.");
+define("TMCEALAN_7", "Use frontend style");
+define("TMCEALAN_8", "When enabled, the editor will use the style of the frontend theme. (when supported)");
+
---
+++ e107_plugins/trackback/languages/German_global.php
@@ -0,0 +1,6 @@
+<?php
+
+define("LAN_PLUGIN_TRACKBACK_NAME", "Trackback");
+define("LAN_PLUGIN_TRACKBACK_DESCRIPTION", "Dieses Plugin erlaubt Ihnen Trackback in Ihren Newsposts zu verwenden.");
+
+
