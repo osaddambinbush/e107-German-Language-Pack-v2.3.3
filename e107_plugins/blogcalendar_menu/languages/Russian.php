df254f0282ba6254163d828cdc141e993adc95b9
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-07 20:05:57 +0200

    Add files via upload

---
+++ e107_plugins/blogcalendar_menu/languages/Russian.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * translated by net1313 $
+ */
+define("BLOGCAL_L1", "Новости по месяцам");
+define("BLOGCAL_L2", "Архив");
+define("BLOGCAL_1", "Новости");
+define("BLOGCAL_CONF1", "Месяцы/ряд");
+define("BLOGCAL_CONF2", "Отступ");
+define("BLOGCAL_CONF4", "Настройки меню календаря");
+define("BLOGCAL_ARCHIV1", "Выбрать архив");
