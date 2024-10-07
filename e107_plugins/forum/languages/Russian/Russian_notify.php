f2503318b65846682ffb6ee41ea0b60f36f057af
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-07 20:24:34 +0200

    Add files via upload

---
+++ e107_plugins/forum/languages/Russian/Russian_notify.php
@@ -0,0 +1,43 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Revision$ 2
+|     $Date$ 03/26/2019
+|     $Author$ net1313 https://e107-ru.com
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_FORUM_NT_7", "Форум - новый пользователь создал тему");
+define("LAN_FORUM_NT_8", "Форум - Тема удалена");
+define("LAN_FORUM_NT_9", "Форум - слияние темы");
+define("LAN_FORUM_NT_10", "Форум - Пост удален");
+define("LAN_FORUM_NT_11", "Форум - сообщение модератору");
+define("LAN_FORUM_NT_12", "Форум - Тема обновлена");
+define("LAN_FORUM_NT_13", "Форум - Тема перемещена");
+define("LAN_FORUM_NT_14", "Форум - Новый пост на форуме");
+define("LAN_FORUM_NT_15", "Форум - Пост обновлен");
+
+define("LAN_FORUM_NT_NEWTOPIC", "Создана новая тема");
+define("LAN_FORUM_NT_NEWTOPIC_PROB", "Тема от юзера с испытательным сроком");
+define("LAN_FORUM_NT_TOPIC_DELETED", "Тема удалена");
+define("LAN_FORUM_NT_TOPIC_SPLIT", "Тема разделена");
+define("LAN_FORUM_NT_TOPIC_UPDATED", "Тема обновлена");
+define("LAN_FORUM_NT_TOPIC_MOVED", "Тема перемещена");
+define("LAN_FORUM_NT_POST_DELETED", "Пост удален");
+define("LAN_FORUM_NT_POST_REPORTED", "Сообщение модератору");
+define("LAN_FORUM_NT_POST_CREATED", "Создан пост в теме");
+define("LAN_FORUM_NT_POST_UPDATED", "Пост обновлен");
+
+
+define("LAN_FORUM_NT_NEWTOPIC_MSG", "Новая тема в форуме [forum] от пользователя: [user]\nТема: [thread]\n\nПост:\n[post]");
+define("LAN_FORUM_NT_NEWTOPIC_PROB_MSG", "Новая тема создана в форуме [forum] новым юзером: [user]\nТема: [thread]\n\nПост:\n[post]");
+define("LAN_FORUM_NT_TOPIC_UPDATED_MSG", "Тема [thread] (в форуме: [forum]) обновлена: [user]");
+define("LAN_FORUM_NT_TOPIC_DELETED_MSG", "Тема [thread] в форуме [forum] удалена: [user]");
+define("LAN_FORUM_NT_TOPIC_MOVED_MSG", "Тема [thread] перемещена из [forum] в [forum2] юзером: [user]");
+
+define("LAN_FORUM_NT_POST_CREATED_MSG", "Новый пост в теме [thread] (Форума: [forum]) автор: [user]\nПост:\n[post]");
+define("LAN_FORUM_NT_POST_UPDATED_MSG", "Пост в теме [thread] (Форума: [forum]) обновлен: [user]\nПост:\n[post]");
+define("LAN_FORUM_NT_POST_DELETED_MSG", "Пост #[postid] темы [thread] в форуме [forum] удален: [user]\n\nПост:\n[post]");
+?>
