6cc8082e72cc0dc0f4ecdcad473565aebb468077
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2024-10-07 20:20:44 +0200

    Add files via upload

---
+++ e107_plugins/featurebox/languages/German_global.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system German Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
+|        $URL: ../e107_plugins/featurebox/languages/German_global.php $
+|        $Revision: 1.0 $
+|        $Id: 2015/07/08 10:28:53 $
+|        $Author: admin $
++---------------------------------------------------------------+
+*/
+define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Zeigt einen animierten Bereich auf dem oberen Rand der Seite mit News - Artikel und andere Inhalte, über den verfügen Sie möchten.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH", "Featurebox Eintrag erstellen.");
+define("FBLAN_INSTALL_01", "Standard Kategorietabelle hinzufügen.");
+define("FBLAN_INSTALL_02", "Standard Kategorietabelle hinzugefügt.");
+define("FBLAN_INSTALL_03", "nicht zugeordnet");
+define("FBLAN_INSTALL_04", "Karussel");
+define("FBLAN_INSTALL_05", "Tabulatoren");
+define("LAN_PLUGIN_FEATUREBOX_RSSFEED", 	"This is the rss feed for the featurebox entries.");
---
+++ e107_plugins/featurebox/languages/Russian_admin_featurebox.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* Copyright (c) e107 Inc 2009 - e107.org, Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+* $Id$
+*
+* Featurebox back-end language defines
+*/
+define("FBLAN_08", "Текст сообщения");
+define("FBLAN_12", "Режим");
+define("FBLAN_13", "Произвольный показ");
+define("FBLAN_14", "Показывать только");
+define("FBLAN_22", "Тип визуализации");
+define("FBLAN_23", "В рамках Темы");
+define("FBLAN_24", "Обычный");
+define("FBLAN_25", "Пока нет элементов, установленных для отображения в теме [x].");
+define("FBLAN_26", "Изображение/видео");
+define("FBLAN_27", "Ссылка для изображения");
+define("FBLAN_28", "Категории меню Featurebox");
+define("FBLAN_29", "Категория для использования в featurebox меню");
+define("FBLAN_30", "Шаблон Категории");
+define("FBLAN_31", "Случайный");
+define("FBLAN_32", "Параметры (необязательно)");
+define("FBLAN_33", "Необязательные параметры Javascript (возможны изменения)");
+define("FBLAN_34", "Не определено");
+define("FBLAN_35", "Карусель");
+define("FBLAN_36", "Вкладки");
